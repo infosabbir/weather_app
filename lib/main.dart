63ccf4861fe2b66cdf7c389c7189123cdd0141a1
infosabbir <infosabbir9@gmail.com>
2025-03-01 09:29:45 +0600

    adding a Elevated Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/loading_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: LoadingScreen(),
+    );
   }
 }
